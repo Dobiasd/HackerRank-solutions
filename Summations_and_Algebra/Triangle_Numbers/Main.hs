0c56912aa51ad1aaf7845e60f937284d93de2818
Dobiasd <harry@daiw.de>
2014-08-18 12:28:52 +0200

    fix: avoid stack overflows with O(1) solution

--- Summations_and_Algebra/Triangle_Numbers/Main.hs
+++ Summations_and_Algebra/Triangle_Numbers/Main.hs
@@ -7,22 +7,29 @@ infixl 0 |>
 main = do
     _ <- getLine
     interact $ unlines .
-               map (show . posOfFirstEvenNumInTriangleRowN . read) .
+               map (show . posOfFirstEvenNumInTriangleRowNFast . read) .
+               filter (not . null) .
                lines
 
 
+posOfFirstEvenNumInTriangleRowNFast :: Int -> Int
+posOfFirstEvenNumInTriangleRowNFast n
+    | n < 3 = -1
+    | otherwise = [2, 3, 2, 4] !! ((n -3) `mod` 4)
+
+
+-- The 'real' solution begins here. But it is not needed.
+
+
 posOfFirstEvenNumInTriangleRowN :: Int -> Int
-posOfFirstEvenNumInTriangleRowN n = triangle !! (n - 1) |> firstPos even |> (+1)
+posOfFirstEvenNumInTriangleRowN n =
+    triangle !! (max 0 (n - 1)) |> findIndex even |> fmap (+1) |> fromMaybe (-1)
 
 
-triangle :: [[Integer]]
+triangle :: [[Int]]
 triangle = iterate nextRow [1]
 
-nextRow :: [Integer] -> [Integer]
-nextRow row = zipWith3 (\a b c -> a+b+c)
+nextRow :: [Int] -> [Int]
+nextRow row = zipWith3 (\ a b c -> a+b+c)
                        (0 : init padded) padded (tail padded ++ [0])
     where padded = 0 : row ++ [0]
-
-
-firstPos :: (a -> Bool) -> [a] -> Int
-firstPos pred xs = fromMaybe (-1) $ findIndex pred xs
