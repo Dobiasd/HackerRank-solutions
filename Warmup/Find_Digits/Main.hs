ab506fdab74cb3ce003d32656e8714fbbc5c19e4
Dobiasd <harry@daiw.de>
2014-08-27 15:09:40 +0200

    Find Digits

---
+++ Warmup/Find_Digits/Main.hs
@@ -0,0 +1,18 @@
+import Control.Arrow
+
+(|>) a b = b a
+infixl 0 |>
+
+main :: IO ()
+main = do
+    _ <- getLine
+    interact $ lines >>> map (solve >>> show) >>> unlines
+
+solve :: String -> Int
+solve s = filter (`divides` n) digits |> length
+    where digits = map ((:[]) >>> read) s
+          n      = read s
+
+divides :: Integer -> Integer -> Bool
+divides 0 _ = False
+divides a b = b `mod` a == 0
