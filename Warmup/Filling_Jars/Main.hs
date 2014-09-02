ff8173636bf8c24168069b31065724a58cab203d
Dobiasd <harry@daiw.de>
2014-09-02 18:12:53 +0200

    Filling Jars

---
+++ Warmup/Filling_Jars/Main.hs
@@ -0,0 +1,22 @@
+import Control.Arrow
+import Control.Applicative
+import Data.List.Split
+
+(|>) a b = b a
+infixl 0 |>
+
+main :: IO ()
+main = do
+    n:_  <- readInts <$> getLine
+    interact $ lines >>> map readInts >>> solve n >>> show >>> (++"\n")
+
+readInts :: String -> [Integer]
+readInts = splitOn " " >>> map read
+
+solve :: Integer -> [[Integer]] -> Integer
+solve n commands = foldl (\x f -> f x) 0 fs |> (`div` n)
+    where fs = map commandToFunc commands
+
+commandToFunc :: [Integer] -> Integer -> Integer
+commandToFunc [a, b, k] = \x -> x + (((b - a) + 1) * k)
+commandToFunc _ = id
